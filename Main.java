e9d78ad2457ad3356e18017019e67f45165907a6
an-Iceberg <a.g.marosi@bluewin.ch>
2022-09-28 01:14:07 +0200

    added playing field

--- Main.java
+++ Main.java
@@ -1,4 +1,37 @@
+import java.util.ArrayList;
+
 class Game {
+  private ArrayList<Integer> PlayingField = new ArrayList<Integer>();
+
+  public Game() {
+    PlayingField.add(0);
+    PlayingField.add(0);
+    PlayingField.add(0);
+
+    PlayingField.add(0);
+    PlayingField.add(0);
+    PlayingField.add(0);
+
+    PlayingField.add(0);
+    PlayingField.add(0);
+    PlayingField.add(0);
+  }
+
+  public void printPlayingField() {
+    System.out.println("+---+---+---+---+");
+    System.out.println("| 1 | 2 | 3 |   |");
+    System.out.println("+---+---+---+---+");
+    System.out.println("|   |   |   | a |");
+    System.out.println("+---+---+---+---+");
+    System.out.println("|   |   |   | b |");
+    System.out.println("+---+---+---+---+");
+    System.out.println("|   |   |   | c |");
+    System.out.println("+---+---+---+---+");
+  }
+
   public static void main(String[] args) {
+    Game game = new Game();
+
+    game.printPlayingField();
   }
 }
